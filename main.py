400b1c374853e53e289322fe4a26397f9e874ec1
ArianN8610 <ariannasiri86@gmail.com>
2023-06-17 10:56:12 +0330

    Correction of check function performance

--- main.py
+++ main.py
@@ -1,42 +1,34 @@
-from flatten import flatten_dict
+def operation(dict_result, value: dict) -> bool:
+    internal_operations = ('==', '!=', '<', '>', '>=', '<=', 'in', 'not in', 'is', 'is not')
+    opera = list(value.keys())[0]
+    new_value = list(value.values())[0]
+
+    if opera in internal_operations:
+        return eval(f'dict_result {opera} new_value')
+    else:
+        ...
 
 
-def check(dictionary: dict, condition: dict) -> bool:
-    operations = ('==', '!=', 'and', 'or')
-    dictionary = flatten_dict(dictionary)
-    bools = set()
+def check(dictionary: dict, condition: dict, parent_key: str = '') -> bool:
+    parent_key = parent_key if parent_key else list(condition.keys())[0]
+    bool_set = set()
 
-    for key, value in flatten_dict(condition).items():
-        result = []
-
-        for i in key.split('.'):
-            if i in operations:
-                if result:
-                    if i == "==":
-                        if dictionary['.'.join(result)] == value:
-                            bools.add(True)
-                        else:
-                            bools.add(False)
-
-                    if i == "!=":
-                        if dictionary['.'.join(result)] != value:
-                            bools.add(True)
-                        else:
-                            bools.add(False)
-            else:
-                result.append(i)
-
-    for i in condition:
-        if i == "and":
-            if False in bools:
-                return False
-            else:
-                return True
+    for key, value in condition.items():
+        if key in ('and', 'or'):
+            bool_set.add(check(dictionary, value, key))
         else:
-            if bools != {False}:
-                return True
-            else:
-                return False
+            dict_result = dictionary
+
+            for i in key.split('.'):
+                dict_result = dict_result[i]
+
+            bool_set.add(operation(dict_result, value))
+
+    if parent_key:
+        if parent_key == 'and':
+            return False not in bool_set
+        else:
+            return True in bool_set
 
 
 if __name__ == '__main__':
@@ -50,19 +42,25 @@ if __name__ == '__main__':
             'city': 'MHD',
             'airport': 'MHD',
             'time': '03:00'
-        }
+        },
+        'a': {'b': {1: 2}},
+        'c': [1, 2]
     }
     b = {
-        'and': {
+        'or': {
+            'a': {'==': {'b': {1: 2}}},
             'and': {
-                'origin.city': {'==': 'THR'},
-                'origin.time': {'==': '02:00'},
-                'or': {
-                    'destination.city': {'!=': 'MHD'},
-                    'destination.time': {'==': '04:00'}
-                }
+                'and': {
+                    'origin.city': {'==': 'THR'},
+                    'origin.time': {'==': '02:00'},
+                    'or': {
+                        'destination.city': {'!=': 'MHD'},
+                        'destination.time': {'==': '04:00'}
+                    }
+                },
+                'origin.airport': {'==': 'IKA'}
             },
-            'origin.airport': {'==': 'IKA'}
+            'c': {'==': [0, 1]}
         }
     }
 
